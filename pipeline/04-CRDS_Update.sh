2b3cabc5d2d6746ca2a84d14290fe6edd1306228
Will Waldron <wwaldron@harding.edu>
2023-05-29 18:38:52 -0500

    Changed CRDS updater to assume in proper env

--- pipeline/04-CRDS_Update.sh
+++ pipeline/04-CRDS_Update.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 # Updates my local CRDS repo for the current image set
-
-# Change Environment
-conda activate stenv
+# Must be run in conda stenv
 
 # Update the References
 # This assumes that the CRDS_SERVER_URL & CRDS_PATH environment variables have been set
-crds bestrefs --files */*flc.fits --sync-references=1 --update-bestrefs
+crds bestrefs --files mastDownload/HST/*/*flc.fits --sync-references=1 --update-bestrefs
